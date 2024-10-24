38eb758c1a9f5d21a17598514f54daa02dbc2262
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2024-10-24 20:44:49 +0900

    [Add]Animation遷移の追加

--- Assets/PlayerDemo.cs
+++ Assets/PlayerDemo.cs
@@ -1,21 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerDemo : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField]
-    int speed = 0 ;    // Start is called before the first frame update
-    void Start()
+    int speed = 0 ;
+    MotionIndex motionIndex;
+    Animator anim;
+    public float waitTime = 0;
+    void Awake()
     {
+        anim = GetComponent<Animator>();
         rb= GetComponent<Rigidbody>();
+        anim.SetInteger("MotionIndex", (int)MotionIndex.Walk);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Moving();
+        AnimationManagement();
+    }
+
+    void Moving()
+    {
         rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+
         
+    }
+
+
+
+    void AnimationManagement()
+    {
+        if(rb.velocity.x >= 0.1f || rb.velocity.x >= 0.1f)
+        {
+            anim.SetInteger("MotionIndex", (int)MotionIndex.Walk);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            waitTime += Time.deltaTime;
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            waitTime = 0f;
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            anim.SetTrigger("Skil");
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            anim.SetTrigger("Ult");
+        }
+
+        if(Input.GetKey(KeyCode.W) && waitTime >= 3f)
+        {
+            anim.SetInteger("MotionIndex", (int)MotionIndex.Run);
+        }
+
+        if(rb.velocity == Vector3.zero)
+        {
+            anim.SetInteger("MotionIndex", (int)MotionIndex.Idol);
+        }
+
+        
+
+    }
+
+    enum MotionIndex 
+    {
+        Idol = 0,Walk = 10,Run = 20, Avoid = 30
     }
 }
