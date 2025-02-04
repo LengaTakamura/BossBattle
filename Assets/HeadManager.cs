5c4939a334658e47fd47f732c57b434a8aac4cef
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-02-04 13:36:53 +0900

    [Add]baseattack

---
+++ Assets/HeadManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class HeadManager : MonoBehaviour
+{
+    public float Damage = 10;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out IDamageable damage) && other.gameObject.tag == "Player")
+        {
+            damage.HitDamage(Damage);
+            Debug.Log("HIT");
+        }
+    }
+}
